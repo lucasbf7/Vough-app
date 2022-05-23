83de49937325466202cea3f4931c9222d06089bb
Lucas Figueiredo <lucasborges_06@hotmail.com>
2022-05-23 14:36:01 -0300

    Projeto inicial com estilização incompleta

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    filter: "",
+    vough: []
+  };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(
+        (res) => {
+        this.setState({
+          vough: res
+        });
+      });
+  }
+
+  handleChange = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Leads</h1>
+        <div>
+          <input placeholder="Pesquise..."></input>
+        </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Cliente</th>
+                <th>Email</th>
+                <th>Telefone</th>
+                <th>Companhia</th>
+                <th>Nicho</th>
+              </tr>
+            </thead>
+            {this.state.vough.map(item => (
+            <tbody key={item.name}>
+              <tr>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+                <td>{item.company.name}</td>
+                <td>{item.company.bs}</td>
+              </tr>
+            </tbody>
+            ))}
+          </table>
+      </div>
+    )
+  }
 }
 
 export default App;
